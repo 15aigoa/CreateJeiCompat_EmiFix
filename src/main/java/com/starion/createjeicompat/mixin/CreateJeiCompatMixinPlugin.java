3714ad42821257afc2a69a53c869366602591052
15aigoa <15a.igoa3danyo@gmail.com>
2026-07-08 23:56:46 +0900

    Update CreateJeiCompatMixinPlugin.java

--- src/main/java/com/starion/createjeicompat/mixin/CreateJeiCompatMixinPlugin.java
+++ src/main/java/com/starion/createjeicompat/mixin/CreateJeiCompatMixinPlugin.java
@@ -1,6 +1,6 @@
 package com.starion.createjeicompat.mixin;
 
-import net.neoforged.fml.ModList;
+import net.minecraftforge.fml.ModList;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
